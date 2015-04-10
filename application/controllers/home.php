05ecfc10352d956f324bbde072b49d4e2125aa9a
Paulo César <jc.cisnerosp@gmail.com>
2015-04-10 09:28:52 -0500

    Code
    Code only in controllers & routes

---
+++ application/controllers/home.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index()
+	{
+		echo "Hola soy el controlador HOME :)";
+	}
+
+	public function mostrar_datos(){
+		echo "Hola soy el controlador mostrar datos :)";
+	}
+
+	public function una_funcion_super_mega_larga(){
+
+	}
+}
+
